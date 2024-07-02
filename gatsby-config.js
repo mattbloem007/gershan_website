416d5d05651e33cb4b7db9413344961eb65cc9b4
Matthew <mattb007@gmail.com>
2024-07-02 20:45:41 +0200

    added netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
     `gatsby-plugin-decap-cms`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
+    `gatsby-plugin-netlify`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
