2195070bc13dfc5d7bce18c58b92b70b23bf4634
Matthew <mattb007@gmail.com>
2024-07-22 16:49:46 +0200

    trying cache clearing

---
+++ netlify/plugin/index.js
@@ -0,0 +1,13 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+module.exports = {
+  async onInit() {
+    console.log('deleting node_modules');
+    await exec('rm -rf node_modules');
+    console.log('clearing yarn cache');
+    await exec('yarn cache clean');
+    console.log('running yarn install');
+    await exec('yarn install');
+  }
+};
