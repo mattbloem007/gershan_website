ad2b167cc868375a1812aa4b96b53d77128a91e2
Matthew <mattb007@gmail.com>
2024-07-22 15:38:17 +0200

    tag edit

--- src/template/tag-template.js
+++ src/template/tag-template.js
@@ -4,10 +4,10 @@ import Post from "../components/post";
 import Layout from "../components/layout";
 
 const TagTemplate = ({data , pageContext}) => {
-    const tagsblogs = data.allMarkdownRemark.edges;
-    const {totalCount} = data.allMarkdownRemark;
-    const {tag} = pageContext;
-    const pageHeader = `${totalCount} post ${(totalCount === 1) ? '':'s'} Tag By <span class="theme-color">${tag}</span>`;
+    // const tagsblogs = data.allMarkdownRemark.edges;
+    // const {totalCount} = data.allMarkdownRemark;
+    // const {tag} = pageContext;
+    // const pageHeader = `${totalCount} post ${(totalCount === 1) ? '':'s'} Tag By <span class="theme-color">${tag}</span>`;
     return (
         <Layout>
             <div className="rn-tag-post-area rn-section-gap bg-color-tertiary">
@@ -15,19 +15,17 @@ const TagTemplate = ({data , pageContext}) => {
                     <div className="row">
                         <div className="col-lg-12">
                             <div className="page-top">
-                                <h1 className="title_holder" dangerouslySetInnerHTML={{ __html: pageHeader }}></h1>
                                 <div className="breadcrumbs-area">
                                     <ul className="breadcrumbs">
                                         <li><a href="/">Home</a></li>
                                         <li className="separator"><span></span></li>
-                                        <li className="active">{tag}</li>
                                     </ul>
                                 </div>
                             </div>
 
                         </div>
                     </div>
-                    <div className="row">
+                    {/**<div className="row">
                         {tagsblogs.map((blog) => (
                         <Post column="col-lg-4 col-md-6 col-12" key={blog.node.fields.slug}
                                 content={{
@@ -37,45 +35,44 @@ const TagTemplate = ({data , pageContext}) => {
                                 }}
                             />
                         ))}
-                    </div>
+                    </div>*/}
                 </div>
             </div>
         </Layout>
     )
 }
 
-export const allTagQueryData = graphql`
-    query allTagQuery ($tag: String!){
-        allMarkdownRemark (
-            sort: {frontmatter: {date: DESC}}
-            filter: {frontmatter: {tags: { in: [$tag]}}}
-        ){
-        totalCount
-        edges {
-            node {
-            id
-            fields {
-                slug
-            }
-            excerpt
-            frontmatter {
-                title
-                format
-                date(formatString: "MMM Do, YYYY")
-                category
-                image {
-                    childImageSharp {
-                        fluid(maxHeight: 300, maxWidth: 500, quality: 100, srcSetBreakpoints: 6) {
-                            ...GatsbyImageSharpFluid_withWebp
-                            presentationWidth
-                            presentationHeight
-                        }
-                    }
-                }
-            }
-            }
-        }
-        }
-    }
-`
+// export const allTagQueryData = graphql`
+//     query allTagQuery ($tag: String!){
+//         allMarkdownRemark (
+//             sort: {frontmatter: {date: DESC}}
+//         ){
+//         totalCount
+//         edges {
+//             node {
+//             id
+//             fields {
+//                 slug
+//             }
+//             excerpt
+//             frontmatter {
+//                 title
+//                 format
+//                 date(formatString: "MMM Do, YYYY")
+//                 category
+//                 image {
+//                     childImageSharp {
+//                         fluid(maxHeight: 300, maxWidth: 500, quality: 100, srcSetBreakpoints: 6) {
+//                             ...GatsbyImageSharpFluid_withWebp
+//                             presentationWidth
+//                             presentationHeight
+//                         }
+//                     }
+//                 }
+//             }
+//             }
+//         }
+//         }
+//     }
+// `
 export default TagTemplate;
